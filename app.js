970d661cb9f2be6b657fb018c579d91e83ba194d
vlakoosh <vlakoosh@gmail.com>
2024-02-15 23:52:42 +0100

    added starter piece layout

--- app.js
+++ app.js
@@ -5,5 +5,12 @@ const infoDisplay = document.querySelector("#infoDisplay");
 const width = 8;
 
 const startPieces = [
-
+    rook,   knight, bishop, queen,  king,   bishop, knight, rook    ,
+    pawn,   pawn,   pawn,   pawn,   pawn,   pawn,   pawn,   pawn    ,
+    '',     '',     '',     '',     '',     '',     '',     ''      ,
+    '',     '',     '',     '',     '',     '',     '',     ''      ,
+    '',     '',     '',     '',     '',     '',     '',     ''      ,
+    '',     '',     '',     '',     '',     '',     '',     ''      ,
+    pawn,   pawn,   pawn,   pawn,   pawn,   pawn,   pawn,   pawn    ,
+    rook,   knight, bishop, queen,  king,   bishop, knight, rook    ,
 ]
