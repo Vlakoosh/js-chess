8c359082f9c6a1fdc1a31a2bd0d0b10bdb885be8
Szymon Walkusz <szymon.walkusz@student.kdg.be>
2024-02-15 23:36:54 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const gameboard = document.querySelector("#player");document.querySelector("#gameboard");
+const currentTurnDisplay = document.querySelector("#player");
+const infoDisplay = document.querySelector("#infoDisplay");
+
+const width = 8;
+
+const startPieces = [
+
+]
