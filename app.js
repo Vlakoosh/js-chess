acc69d40df07334cdb3fbbe81ded9f9af42e4085
vlakoosh <vlakoosh@gmail.com>
2024-02-16 13:50:05 +0100

    update. board working, pieces and board colored draggable pieces

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const gameboard = document.querySelector("#player");document.querySelector("#gameboard");
+const gameboard =document.querySelector("#gameboard");
 const currentTurnDisplay = document.querySelector("#player");
 const infoDisplay = document.querySelector("#infoDisplay");
 
@@ -14,3 +14,49 @@ const startPieces = [
     pawn,   pawn,   pawn,   pawn,   pawn,   pawn,   pawn,   pawn    ,
     rook,   knight, bishop, queen,  king,   bishop, knight, rook    ,
 ]
+
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
+        const square = document.createElement('div');
+        square.innerHTML = startPiece;
+        square.firstChild && square.firstChild.setAttribute('draggable', true)
+        square.setAttribute('square-id', i);
+
+        const row = Math.floor( (63 - i) / 8) + 1;
+        if (row % 2 === 0){
+            square.classList.add(i % 2 === 0 ? "black" : "white")
+        }
+        else {
+            square.classList.add(i % 2 === 0 ? "white" : "black")
+        }
+        square.classList.add('square');
+
+        if (i < 16) {
+            square.firstChild.firstChild.classList.add('black-piece')
+        }
+        if (64 - i <= 16){
+            square.firstChild.firstChild.classList.add('white-piece')
+        }
+
+        gameboard.appendChild(square);
+    })
+}
+
+createBoard();
+
+
+
+
+const allSquares = document.querySelectorAll("#gameboard .square");
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart)
+})
+
+let startPosition;
+let draggedElement;
+
+function dragStart (e) {
+    startPosition = e.target.parentNode.getAttribute('square-id');
+    draggedElement = e.target
+}
